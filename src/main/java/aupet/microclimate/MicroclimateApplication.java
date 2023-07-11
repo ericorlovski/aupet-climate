a79815d6a8e52a3ed12b076513ba116dabd072dc
KUSAINAN <anuar.kusainov@bcc.kz>
2023-07-11 17:52:22 +0600

    project skeleton

---
+++ src/main/java/aupet/microclimate/MicroclimateApplication.java
@@ -0,0 +1,13 @@
+package aupet.microclimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroclimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroclimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aupet/microclimate/MicroclimateApplicationTests.java
@@ -0,0 +1,13 @@
+package aupet.microclimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroclimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
