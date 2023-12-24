119fc792f2809cc5626ba1e5a29c87ecdc355672
KUSAINAN <anuar.kusainov@bcc.kz>
2023-12-25 00:07:01 +0600

    Added new column to page

--- src/main/java/aupet/microclimate/controller/AdminPageController.java
+++ src/main/java/aupet/microclimate/controller/AdminPageController.java
@@ -4,8 +4,10 @@ import aupet.microclimate.model.repository.HumTempRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,10 +17,10 @@ import org.springframework.web.servlet.ModelAndView;
 @RequiredArgsConstructor
 public class AdminPageController {
 
-    private final HumTempRepository climateRepository;
+    private final HumTempRepository humTempRepository;
     @GetMapping(value = "/")
     public ModelAndView getAdminPage(Model model) {
-        val entity = climateRepository.findAll();
+        val entity = humTempRepository.findAll();
 
         model.addAttribute("climate", entity);
         model.addAttribute("formErrors", 0);
@@ -26,4 +28,11 @@ public class AdminPageController {
         return new ModelAndView("climate","model", model);
     }
 
+    @GetMapping(value = "/toggle_climate/{id}")
+    @Transactional(rollbackFor = Exception.class)
+    public ModelAndView toggleCategoryActivity(@PathVariable Long id) {
+        val cat = humTempRepository.findById(id);
+        cat.ifPresent(food -> food.setActive(!food.isActive()));
+        return new ModelAndView("redirect:/climate/");
+    }
 }
